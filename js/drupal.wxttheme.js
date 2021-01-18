884f51127ce45bfe6ef7b7521bbe9dd5c02ec406
Steve McNabb <steve.mcnabb@canada.ca>
2021-01-18 09:31:54 -0500

    theme

---
+++ js/drupal.wxttheme.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * WxTTheme behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.wxttheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
